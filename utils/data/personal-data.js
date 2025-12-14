3896a60e8a5123615b88525cb8c04116ff4434c1
SHIVAM NEGI <shivamnegi@SHIVAMs-MacBook-Air.local>
2025-12-14 23:26:14 +0530

    added profile photo

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,6 +1,6 @@
 export const personalData = {
   name: "Shivam Negi",
-  profile: '/profile.png',
+  profile: '/profile.jpg',
   designation: "Software Developer",
   description: "My name is Shivam Negi. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
   email: 'shivamnegi896@gmail.com',
