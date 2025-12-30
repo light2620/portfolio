50937cab98866fb2b58ffb26b7fe3eacd7247017
shivam negi <92973392+light2620@users.noreply.github.com>
2025-12-30 17:14:15 +0530

    Fix address formatting in personal-data.js
    Removed trailing comma from the address field.

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -5,7 +5,7 @@ export const personalData = {
   description: "My name is Shivam Negi, and I am a Full-Stack Developer passionate about building scalable and user-centric web applications. I am a quick learner with a self-driven mindset and a strong interest in both frontend and backend development.I work across the full web stack, from creating responsive user interfaces to developing efficient server-side logic and APIs. My core expertise is in JavaScript, and I actively seek full-stack opportunities that support continuous learning and professional growth.",
   email: 'shivamnegi896@gmail.com',
   phone: '+918076228983',
-  address: 'Faridabad, Haryana, 121001,',
+  address: 'Faridabad, Haryana, 121001',
   github: 'https://github.com/light2620',
   facebook: 'https://www.facebook.com/abusaid.riyaz/',
   linkedIn: 'https://www.linkedin.com/in/shivam-negi-light/',
